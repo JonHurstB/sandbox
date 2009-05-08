712ef9e811adf96190acbb6ad98329b2c879b786
JonHurst <jon.a@hursts.eclipse.co.uk>
2009-05-08 11:55:54 +0100

    Changes to accomodate Windows junctions

---
+++ make_junctions.bat
@@ -0,0 +1,19 @@
+mkdir site
+mkdir site/script
+junction site/script script
+mkdir site/stylesheets
+junction site/stylesheets stylesheets
+mkdir site/images
+junction site/images images
+mkdir site/photos
+junction site/photos photos
+
+mkdir pages/script
+junction pages/script script
+mkdir pages/stylesheets
+junction pages/stylesheets stylesheets
+mkdir pages/images
+junction pages/images images
+mkdir pages/photos
+junction pages/photos photos
+
---
+++ make_symlinks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir site
+ln -s ../stylesheets ../script ../images ../photos site/
+ln -s ../stylesheets ../script ../images ../photos pages/
