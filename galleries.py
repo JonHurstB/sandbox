03b602fbcd769abc332dc033f730f3a9f96717eb
JonHurst <jon.a@hursts.eclipse.co.uk>
2009-06-02 08:05:53 +0100

    Bishop's service added

--- galleries.py
+++ galleries.py
@@ -17,3 +17,26 @@ captions = {
         "<p>Our wildlife pond. A favoured haunt of our local heron.</p>",
     "img_1077.jpg":
         "<p>Foundation playground. Completed in 2008. A fantastic new resource for our youngest students.</p>"}
+
+
+#Bishop Peter's service
+
+structure.append(
+    ["Bishop's Service", "bishops_service",
+     ["dgm3cn8j_47d44s2jd4_b.jpg",
+      "dgm3cn8j_40c3dmftf7_b.jpg",
+      "dgm3cn8j_41d46dd7gb_b.jpg",
+      "dgm3cn8j_49h436vfhd_b.jpg"]])
+
+captions.update({
+        "dgm3cn8j_47d44s2jd4_b.jpg":
+            ("<p>The Diocese of Bath and Wells are celebrating their 11th centenary. Bishop Peter Price plans"
+             " to walk around the entire diocese visiting every deanery.</p>"),
+        "dgm3cn8j_40c3dmftf7_b.jpg":
+            "<p>The twelve Church of England Schools in the deanery were invited ...</p>",
+        "dgm3cn8j_41d46dd7gb_b.jpg":
+            "<p>... to a service at St Paul's Church Weston Super Mare.</p>",
+        "dgm3cn8j_49h436vfhd_b.jpg":
+            ("<p>Each school had to offer their own presentation on 'celebrating what it means to be a C of E school"
+             " in this Diocese'. Year 5 created a rap.</p>")
+})
